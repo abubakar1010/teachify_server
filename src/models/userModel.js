56a2aab3c7bbae0a40e66e3b6a830fc7cf8ed25d
abu bakar <abubakar850772@gmail.com>
2024-09-29 14:20:00 +0600

    add user model

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,9 +1,59 @@
-import { Model, Schema } from "mongoose";
+import { model, Schema } from "mongoose";
 
-const UserSchema = new Schema({
+const UserSchema = new Schema(
+    {
 
-})
+        name: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        bio: {
+            type: String,
+            required: true
+        },
+        profileImage: {
+            type: String,
+            required: true
+        },
+        role: {
+            type: String,
+            enum: ["student", "teacher", "admin"],
+            default: "student"
+        },
+        enrolledCourses: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Enrollment"
+            }
+        ],
+        isVerified: {
+            type: Boolean,
+            required: true,
+            default: false
+        },
+        subscriptionStatus: {
+            type: Boolean,
+            required: true,
+            default: false
+        },
+        isBanned: {
+            type: Boolean,
+            required: true,
+            default: false
+        }
 
-const User = Model("User", UserSchema)
+    },
+    {
+        timestamps: true
+    }
+)
 
-export default User;
+export const User = model("User", UserSchema)
