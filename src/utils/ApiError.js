5944b9b38820343eff6aaca8064a5985e1f6b354
abu bakar <abubakar850772@gmail.com>
2024-09-29 14:29:23 +0600

    add custom error handlers

---
+++ src/utils/ApiError.js
@@ -0,0 +1,26 @@
+class ApiError extends Error {
+	constructor(
+		statusCode,
+		message = "something went wrong",
+		error = [],
+		stack = ""
+	) {
+        super(message)
+        this.statusCode = statusCode,
+        this.error = error,
+        this.message = message,
+        this.data = null,
+        this.success = false
+
+        if(this.stack){
+            this.stack = stack
+        }else{
+            Error.captureStackTrace(this, this.constructor)
+        }
+
+        
+    }
+}
+
+
+export default ApiError
